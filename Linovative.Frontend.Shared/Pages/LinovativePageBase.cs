cba3bff4ccc1f8c995861665b6a28e742a6d78f4
Vinsensius Pance <Vinsensius_P908@mitrais.com>
2025-10-27 17:30:13 +0800

    Create base page

---
+++ Linovative.Frontend.Shared/Pages/LinovativePageBase.cs
@@ -0,0 +1,23 @@
+﻿using Linovative.Frontend.Services.Interfaces;
+using Microsoft.AspNetCore.Components;
+
+namespace Linovative.Frontend.Shared.Pages
+{
+    public abstract class LinovativePageBase : ComponentBase
+    {
+        [Inject]
+        public IJsonLocalizer JsonLocalizer { get; set; }
+
+        protected abstract string LocalizerKey { get; }
+        protected string Lang(string key, params object[] args) 
+            => args?.Count() == 0?  
+            JsonLocalizer[$"{LocalizerKey}.key"] 
+            : JsonLocalizer.Format(key, args);
+
+        protected override async Task OnInitializedAsync()
+        {
+            await JsonLocalizer.EnsureLoadedAsync(LocalizerKey); 
+            await base.OnInitializedAsync();
+        }
+    }
+}
