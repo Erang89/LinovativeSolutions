e53a03202d71c21d678f745c3e6edd601e5da816
Vinsensius Pance <Vinsensius_P908@mitrais.com>
2025-11-06 03:33:49 +0800

    Bulk delete working shift

---
+++ LinoVative.Service.Backend/CrudServices/Shifts/BulkDeleteShiftCommand.cs
@@ -0,0 +1,23 @@
+﻿using Linovative.Shared.Interface;
+using LinoVative.Service.Backend.Interfaces;
+using LinoVative.Service.Core.Interfaces;
+using LinoVative.Service.Core.Shifts;
+using LinoVative.Shared.Dto;
+using MapsterMapper;
+using Microsoft.Extensions.Localization;
+
+namespace LinoVative.Service.Backend.CrudServices.Outlets.Shifts
+{
+    public class BulkDeleteShiftCommand : IRequest<Result>, IBulkDeleteDto
+    {
+        public List<Guid> Ids { get; set; } = new();
+    }
+
+    public class BulkDeleteShiftHandlerService : SaveDeleteServiceBase<Shift, BulkDeleteShiftCommand>, IRequestHandler<BulkDeleteShiftCommand, Result>
+    {
+        public BulkDeleteShiftHandlerService(IAppDbContext dbContext, IActor actor, IMapper mapper, IAppCache appCache, IStringLocalizer localizer) : 
+            base(dbContext, actor, mapper, appCache, localizer)
+        {
+        }
+    }
+}
--- LinoVative.Web.Api/Areas/Admin/Controllers/Shifts/ShiftsController.cs
+++ LinoVative.Web.Api/Areas/Admin/Controllers/Shifts/ShiftsController.cs
@@ -3,6 +3,8 @@ using LinoVative.Shared.Dto;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using LinoVative.Service.Backend.CrudServices.Shifts;
+using LinoVative.Service.Backend.CrudServices.Outlets.Outlets;
+using LinoVative.Service.Backend.CrudServices.Outlets.Shifts;
 
 namespace LinoVative.Web.Api.Areas.Admin.Controllers.Shifts
 {
@@ -62,6 +64,27 @@ namespace LinoVative.Web.Api.Areas.Admin.Controllers.Shifts
             try
             {
                 var result = await _mediator.Send(new DeleteShiftCommand() { Id = id}, token);
+                return StatusCode((int)result.Status, result);
+            }
+            catch (Exception ex)
+            {
+                var routeName = ControllerContext.ActionDescriptor.DisplayName;
+                _logger.LogError(ex, LOG_ERRROR_MESSAGE, routeName);
+                var responseObject = Result.Failed(string.Format(DISPLAY_ERROR_MESSAGE, routeName));
+                responseObject.SetTraceId(HttpContext.TraceIdentifier);
+                return StatusCode((int)HttpStatusCode.InternalServerError, responseObject)!;
+            }
+        }
+
+
+        [Route(BULKDELETE)]
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> DeleteAll([FromBody] BulkDeleteShiftCommand cmd, CancellationToken token)
+        {
+            try
+            {
+                var result = await _mediator.Send(cmd, token);
                 return StatusCode((int)result.Status, result);
             }
             catch (Exception ex)
