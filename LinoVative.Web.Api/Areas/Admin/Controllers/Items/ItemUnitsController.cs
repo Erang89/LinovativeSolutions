2efef8763b2bbabc731342db958b6a3289d1397b
Vinsensius Pance <Vinsensius_P908@mitrais.com>
2025-11-05 13:32:35 +0800

    Item Units Page

--- LinoVative.Web.Api/Areas/Admin/Controllers/Items/ItemUnitsController.cs
+++ LinoVative.Web.Api/Areas/Admin/Controllers/Items/ItemUnitsController.cs
@@ -3,6 +3,7 @@ using LinoVative.Shared.Dto;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using LinoVative.Service.Backend.CrudServices.Items.Units;
+using LinoVative.Service.Backend.CrudServices.Items.ItemGroups;
 
 namespace LinoVative.Web.Api.Areas.Admin.Controllers.Items
 {
@@ -62,6 +63,27 @@ namespace LinoVative.Web.Api.Areas.Admin.Controllers.Items
             try
             {
                 var result = await _mediator.Send(new DeleteItemUnitCommand() { Id = id}, token);
+                return StatusCode((int)result.Status, result);
+            }
+            catch (Exception ex)
+            {
+                var routeName = ControllerContext.ActionDescriptor.DisplayName;
+                _logger.LogError(ex, LOG_ERRROR_MESSAGE, routeName);
+                var responseObject = Result.Failed(string.Format(DISPLAY_ERROR_MESSAGE, routeName));
+                responseObject.SetTraceId(HttpContext.TraceIdentifier);
+                return StatusCode((int)HttpStatusCode.InternalServerError, responseObject)!;
+            }
+        }
+
+
+        [Route(BULKDELETE)]
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> DeleteAll([FromBody] BulkDeleteItemUnitCommand cmd, CancellationToken token)
+        {
+            try
+            {
+                var result = await _mediator.Send(cmd, token);
                 return StatusCode((int)result.Status, result);
             }
             catch (Exception ex)
---
+++ Linovative.Frontend.Services/FrontendServices/ItemUnitService.cs
@@ -0,0 +1,20 @@
+﻿using Linovative.Frontend.Services.FrontendServices.BaseServices;
+using Linovative.Frontend.Services.Interfaces;
+using LinoVative.Shared.Dto.ItemDtos;
+using Microsoft.Extensions.Logging;
+
+namespace Linovative.Frontend.Services.FrontendServices
+{
+    public interface IItemUnitService : IReadOnlyService<ItemUnitDto>, ICrudInterfaces
+    {
+
+    }
+
+    public class ItemUnitService : CrudServiceAbstract<ItemUnitDto>, IItemUnitService
+    {
+        public ItemUnitService(IHttpClientFactory httpFactory, ILogger<ItemUnitService> logger) : base(httpFactory, logger, "ItemUnits")
+        {
+        }
+
+    }
+}
