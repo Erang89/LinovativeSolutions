0199dd93ad785e312ce972e8fc15dade8273b776
Vinsensius Pance <Vinsensius_P908@mitrais.com>
2025-11-10 14:02:47 +0800

    Accounting Index Page

---
+++ Linovative.Frontend.AccountingComponents/Bases/AccountingPageBase.cs
@@ -0,0 +1,15 @@
+﻿using Linovative.Frontend.Shared.Pages;
+
+namespace Linovative.Frontend.AccountingComponents.Bases
+{
+    public class AccountingPageBase : LinovativePageBase
+    {
+        protected override string? LibraryName => "Linovative.Frontend.AccountingComponents";
+        public override string Label(string key) => JsonLocalizer[$"{LocalizerResource}.{key}.Label"];
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+        }
+    }
+}
