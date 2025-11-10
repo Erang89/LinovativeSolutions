4a0f59a4831583cfd1fa6bcbf40033a44ebbba4c
Vinsensius Pance <Vinsensius_P908@mitrais.com>
2025-11-10 15:21:19 +0800

    Refactor services

---
+++ Linovative.Frontend.Services/FrontendServices/AccountingServices/AccountService.cs
@@ -0,0 +1,20 @@
+﻿using Linovative.Frontend.Services.FrontendServices.BaseServices;
+using Linovative.Frontend.Services.Interfaces;
+using LinoVative.Shared.Dto.MasterData.Accountings;
+using Microsoft.Extensions.Logging;
+
+namespace Linovative.Frontend.Services.FrontendServices.AccountingServices
+{
+    public interface IAccountService : IReadOnlyService<AccountViewDto>, ICrudInterfaces
+    {
+
+    }
+
+    public class AccountService : CrudServiceAbstract<AccountViewDto>, IAccountService
+    {
+        public AccountService(IHttpClientFactory httpFactory, ILogger<AccountService> logger) : base(httpFactory, logger, "Accounts")
+        {
+        }
+
+    }
+}
