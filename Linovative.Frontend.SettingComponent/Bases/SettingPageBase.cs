e7e81560a528c5b744650e5e7924d29ed1a0c6a1
Vinsensius Pance <Vinsensius_P908@mitrais.com>
2025-11-11 11:17:36 +0800

    Settings Component

---
+++ Linovative.Frontend.SettingComponent/Bases/SettingPageBase.cs
@@ -0,0 +1,15 @@
+﻿using Linovative.Frontend.Shared.Pages;
+
+namespace Linovative.Frontend.SettingComponent.Bases
+{
+    public class SettingPageBase : LinovativePageBase
+    {
+        protected override string? LibraryName => "Linovative.Frontend.SettingComponent";
+        public override string Label(string key) => JsonLocalizer[$"{LocalizerResource}.{key}.Label"];
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+        }
+    }
+}
