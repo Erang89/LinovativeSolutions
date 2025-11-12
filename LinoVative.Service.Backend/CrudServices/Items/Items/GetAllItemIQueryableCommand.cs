299a4d8cb1adaae007e9f21b31202c49853bfce4
Vinsensius Pance <Vinsensius_P908@mitrais.com>
2025-11-12 11:43:11 +0800

    Add dinamyc filter to item

--- LinoVative.Service.Backend/CrudServices/Items/Items/GetAllItemIQueryableCommand.cs
+++ LinoVative.Service.Backend/CrudServices/Items/Items/GetAllItemIQueryableCommand.cs
@@ -1,7 +1,9 @@
 ﻿using Linovative.Shared.Interface;
+using LinoVative.Service.Backend.Extensions;
 using LinoVative.Service.Backend.Interfaces;
 using LinoVative.Service.Core.Interfaces;
 using LinoVative.Service.Core.Items;
+using LinoVative.Shared.Dto.Commons;
 using LinoVative.Shared.Dto.ItemDtos;
 using Mapster;
 using MapsterMapper;
@@ -11,6 +13,7 @@ namespace LinoVative.Service.Backend.CrudServices.Items.Items
     public class GetAllItemIQueryableCommand : IRequest<IQueryable<ItemViewDto>>
     {
         public string? SearchKeyword { get; set; }
+        public List<FilterCondition> Filter { get; set; } = new();
     }
 
     public class GetAllItemQueryableHandlerService : QueryServiceBase<Item, GetAllItemIQueryableCommand>, IRequestHandler<GetAllItemIQueryableCommand, IQueryable<ItemViewDto>>
@@ -25,6 +28,6 @@ namespace LinoVative.Service.Backend.CrudServices.Items.Items
         }
 
         public Task<IQueryable<ItemViewDto>> Handle(GetAllItemIQueryableCommand request, CancellationToken ct) 
-            => Task.FromResult(base.GetAll(request).ProjectToType<ItemViewDto>(_mapper.Config));
+            => Task.FromResult(base.GetAll(request).ProjectToType<ItemViewDto>(_mapper.Config).ApplyFilters(request.Filter));
     }
 }
--- LinoVative.Shared.Dto/Enums/FilterOperator.cs
+++ LinoVative.Shared.Dto/Enums/FilterOperator.cs
@@ -1,36 +1,20 @@
-﻿using LinoVative.Shared.Dto.Attributes;
-
-namespace LinoVative.Shared.Dto.Enums
+﻿namespace LinoVative.Shared.Dto.Enums
 {
     public enum FilterOperator
     {
-        [EnumDisplayName("equals")]
         Equals = 1,
-        [EnumDisplayName("not equals")]
         NotEquals = 2,
-        [EnumDisplayName("contains")]
         Contains = 3,
-        [EnumDisplayName("not contains")]
         NotContains = 4,
-        [EnumDisplayName("starts with")]
         StartsWith = 5,
-        [EnumDisplayName("ends with")]
         EndsWith = 6,
-        [EnumDisplayName("greater than")]
         GreaterThan = 7,
-        [EnumDisplayName("greater than or equal")]
         GreaterThanOrEqual = 8,
-        [EnumDisplayName("less than")]
         LessThan = 9,
-        [EnumDisplayName("less than or equal")]
         LessThanOrEqual = 10,
-        [EnumDisplayName("is empty")]
         IsNull = 11,
-        [EnumDisplayName("is not empty")]
         IsNotNull = 12,
-        [EnumDisplayName("in")]
         In = 13,
-        [EnumDisplayName("not in")]
         NotIn = 14
     }
 }
