be747b9e85fd70146bb18d557313ce877ef10bdb
Vinsensius Pance <Vinsensius_P908@mitrais.com>
2025-11-13 16:20:56 +0800

    Advance Item Operations

---
+++ Linovative.Frontend.POSComponents/Enums/MasterItemBulkOperations.cs
@@ -0,0 +1,31 @@
+﻿namespace Linovative.Frontend.POSComponents.Enums
+{
+    public enum DataEntityTypes
+    {
+        ItemMaster,
+        Group,
+        Category,
+        Unit
+    }
+
+    public enum Operations
+    {
+        Create,
+        Update,
+        Delete
+    }
+
+    public enum ActionTypes
+    {
+        Create_DownloadTemplate,
+        Create_Upload,
+        Update_DownloadBlankTemplate,
+        Update_DownloadFromData,
+        Update_DownloadFromDataMapping,
+        Update_Upload,
+        Delete_DownloadBlankTemplate,
+        Delete_DownloadFromDataFiltering,
+        Delete_DownloadFromDataMapping,
+        Delete_Upload
+    }
+}
