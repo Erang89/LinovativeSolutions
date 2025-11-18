055ac84300d7fb26fbe0c06e5a27ce9c6bb662eb
Vinsensius Pance <Vinsensius_P908@mitrais.com>
2025-11-18 14:42:40 +0800

    Practicing Recording with PlayWrite

---
+++ POSManagementTest/PageTestBase.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.Xunit;
+
+namespace POSManagementTest
+{
+    public class PageTestBase : PageTest
+    {
+        protected Uri RootUri => new Uri("https://localhost:7081");
+    }
+}
--- POSManagementTest/UnitTest1.cs
+++ POSManagementTest/UnitTest1.cs
@@ -1,18 +1,26 @@
-using Microsoft.Playwright.Xunit;
 using Microsoft.Playwright;
 using System.Text.RegularExpressions;
 
 namespace POSManagementTest;
 
-public class UnitTest1 : PageTest
+public class UnitTest1 : PageTestBase
 {
+    
+
     [Fact]
     public async Task HasTitle()
-    {
+    {        
         await Page.GotoAsync("https://playwright.dev");
-
+        await Page.PauseAsync();
         // Expect a title "to contain" a substring.
         await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
+
+        await Page.GetByRole(AriaRole.Heading, new() { Name = "Playwright enables reliable" }).Locator("span").ClickAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Get started" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Heading, new() { Name = "Installation" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Writing tests", Exact = true }).ClickAsync();
+        await Page.GetByRole(AriaRole.Heading, new() { Name = "First testDirect link to" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Emulation" }).ClickAsync();
     }
 
     [Fact]
@@ -25,5 +33,6 @@ public class UnitTest1 : PageTest
 
         // Expects page to have a heading with the name of Installation.
         await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Installation" })).ToBeVisibleAsync();
+
     }
 }
