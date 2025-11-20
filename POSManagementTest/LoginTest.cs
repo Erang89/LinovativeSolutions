2843669fc482e2a10d5286726654a2231d581fb8
Vinsensius Pance <Vinsensius_P908@mitrais.com>
2025-11-20 12:12:33 +0800

    Start Test Login Page

---
+++ POSManagementTest/LoginTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Playwright;
+
+namespace POSManagementTest
+{
+    public class LoginTest : PageTestBase
+    {
+        [Fact]
+        public async Task ValidLogin()
+        {
+            await Page.GotoAsync($"{RootUri}login");
+
+            await Page.GetByRole(AriaRole.Textbox, new() { Name = "User Name*" }).ClickAsync();
+            await Page.GetByRole(AriaRole.Textbox, new() { Name = "User Name*" }).FillAsync("vincent@linovative.com");
+            await Page.GetByRole(AriaRole.Textbox, new() { Name = "Password*" }).ClickAsync();
+            await Page.GetByRole(AriaRole.Textbox, new() { Name = "Password*" }).FillAsync("NotSecure@1");
+            await Page.GetByRole(AriaRole.Button, new() { Name = "Login" }).ClickAsync();
+            await Expect(Page).ToHaveURLAsync($"{RootUri}");
+        }
+
+        [Fact]
+        public async Task InvalidLoginTest()
+        {
+            await Page.GotoAsync($"{RootUri}login");
+            
+            // Set 1
+            await Page.GetByRole(AriaRole.Textbox, new() { Name = "User Name*" }).ClickAsync();
+            await Page.GetByRole(AriaRole.Textbox, new() { Name = "User Name*" }).FillAsync("user@linovative.com");
+            await Page.GetByRole(AriaRole.Textbox, new() { Name = "Password*" }).FillAsync("1");
+            await Page.GetByRole(AriaRole.Button, new() { Name = "Login" }).ClickAsync();
+            var errorLocator = Page.Locator(".mud-alert-message");
+            // Assert 1
+            await Expect(errorLocator).ToContainTextAsync("Incorect username or password");
+            await Expect(Page).ToHaveURLAsync($"{RootUri}login");
+           
+
+            // Set 2
+            await Page.GetByRole(AriaRole.Textbox, new() { Name = "Password*" }).FillAsync("NotSecure@1");
+            await Page.GetByRole(AriaRole.Textbox, new() { Name = "User Name*" }).FillAsync("");
+            await Page.GetByRole(AriaRole.Button, new() { Name = "Login" }).ClickAsync();
+            await Page.GetByText("User Name is required").ClickAsync();
+            await Page.GetByRole(AriaRole.Textbox, new() { Name = "User Name*" }).FillAsync("xxx");
+            await Page.GetByRole(AriaRole.Button, new() { Name = "Login" }).ClickAsync();
+            errorLocator = Page.Locator(".mud-alert-message");
+            // Assert 2
+            await Expect(errorLocator).ToContainTextAsync("Incorect username or password");
+            await Expect(Page).ToHaveURLAsync($"{RootUri}login");
+        }
+    }
+}
