027510f1a3d9e2987776389824a2966fca3fd46d
Vinsensius Pance <Vinsensius_P908@mitrais.com>
2025-11-26 11:12:39 +0800

    Test Delete Unit

--- Linovative.BackendTest/Test.Service.Backend/CrudService/Items/Units/UnitCRUDTest.cs
+++ Linovative.BackendTest/Test.Service.Backend/CrudService/Items/Units/UnitCRUDTest.cs
@@ -8,7 +8,11 @@ namespace Linovative.BackendTest.Test.Service.Backend.CrudService.Items.Units
         [Fact]
         public async Task CRUD_Success()
         {
-            // Arrange Insert
+            /*+++++++++++++++++++++++++++++++++++++++++++++++++++
+             *+ INSERT ITEM
+             *++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+            // Arrange
             var dbContext = CreateContext();
             var cts = new CancellationTokenSource();
             var createHandler = new CreateItemUnitHandlerService(dbContext, _actor, _mapper, _appCache, _localizer);
@@ -19,11 +23,11 @@ namespace Linovative.BackendTest.Test.Service.Backend.CrudService.Items.Units
             };
 
 
-            // Action Insert
+            // Action
             await createHandler.Handle(createCommand, cts.Token);
             var unit = dbContext.ItemUnits.Where(x => x.Id == createCommand.Id).FirstOrDefault();
             
-            // Assert Insert
+            // Assert
             Assert.NotNull(unit);
             Assert.Equal(createCommand.Name, unit.Name);
             Assert.False(unit.IsDeleted);
@@ -33,18 +37,44 @@ namespace Linovative.BackendTest.Test.Service.Backend.CrudService.Items.Units
             Assert.Null(unit.LastModifiedBy);
             Assert.Equal(unit.CompanyId, _actor.CompanyId);
 
-            // Arrange Update
+
+            /*+++++++++++++++++++++++++++++++++++++++++++++++++++
+             *+ UPDATE ITEM
+             *++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+            // Arrange
             var updateHandler = new UpdateItemUnitHandlerService(dbContext, _actor, _mapper, _appCache, _localizer);
             var updateCommand = new UpdateItemUnitCommand() { Id = createCommand.Id, Name = "Test Update" };
             
-            // Action: Update
+            // Action
             var updateResult = await updateHandler.Handle(updateCommand, cts.Token);
             unit = dbContext.ItemUnits.FirstOrDefault(x => x.Id == updateCommand.Id);
 
-            // Assert : Update
+            // Assert
             Assert.True(updateResult);
             Assert.NotNull(unit);
             Assert.Equal(unit.Name, updateCommand.Name);
+            Assert.NotNull(unit.LastModifiedAtUtcTime);
+            Assert.NotNull(unit.LastModifiedBy);
+            Assert.Equal(unit.LastModifiedBy, _actor.UserId);
+
+
+
+
+            /*+++++++++++++++++++++++++++++++++++++++++++++++++++
+             *+ DELETE ITEM
+             *++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+            //Arrange
+            var deleteHandler = new DeleteItemUnitHandlerService(dbContext, _actor, _mapper, _appCache, _localizer);
+            var deleteCommand = new DeleteItemUnitCommand() { Id = createCommand.Id };
+
+            // Action
+            await deleteHandler.Handle(deleteCommand, cts.Token);
+            unit = dbContext.ItemUnits.FirstOrDefault(x => x.Id == updateCommand.Id);
+
+            // Assert
+            Assert.True(unit!.IsDeleted);
             Assert.NotNull(unit.LastModifiedAtUtcTime);
             Assert.NotNull(unit.LastModifiedBy);
             Assert.Equal(unit.LastModifiedBy, _actor.UserId);
