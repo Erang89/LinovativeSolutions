7b327ebf36f23ffdf56412bf7e7f7c7a8d7d7797
Vinsensius Pance <Vinsensius_P908@mitrais.com>
2025-12-01 14:18:17 +0800

    Fixing Bulk Upload Group

---
+++ Linovative.Frontend.Services/FrontendServices/BulkUploads/BulkUploadItemGroupDetailService.cs
@@ -0,0 +1,19 @@
+﻿using Linovative.Frontend.Services.FrontendServices.BaseServices;
+using Linovative.Frontend.Services.Interfaces;
+using LinoVative.Shared.Dto.BulkUploads;
+using Microsoft.Extensions.Logging;
+
+namespace Linovative.Frontend.Services.BulkUploads
+{
+    public interface IBulkUploadItemGroupDetailService : IReadOnlyService<BulkUploadItemGroupDetailDto>, ICrudInterfaces
+    {
+
+    }
+
+    public class BulkUploadItemGroupDetailService : CrudServiceAbstract<BulkUploadItemGroupDetailDto>, IBulkUploadItemGroupDetailService
+    {
+        public BulkUploadItemGroupDetailService(IHttpClientFactory httpFactory, ILogger<BulkUploadItemGroupDetailService> logger) : base(httpFactory, logger, "BulkUploadItemGroupDetails")
+        {
+        }
+    }
+}
