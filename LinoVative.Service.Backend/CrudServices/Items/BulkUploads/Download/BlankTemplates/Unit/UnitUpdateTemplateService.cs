dc0d77779d793e919ad4e77c1c6a4886b2263077
Vinsensius Pance <Vinsensius_P908@mitrais.com>
2025-12-09 12:02:20 +0800

    Fixing get template for bulk operation

---
+++ LinoVative.Service.Backend/CrudServices/Items/BulkUploads/Download/BlankTemplates/Unit/UnitUpdateTemplateService.cs
@@ -0,0 +1,28 @@
+﻿using ClosedXML.Excel;
+using LinoVative.Service.Backend.CrudServices.Items.BulkUploads.Download.BlankTemplates.Base;
+using LinoVative.Service.Backend.Interfaces;
+
+namespace LinoVative.Service.Backend.CrudServices.Items.BulkUploads.Download.BlankTemplates.Unit
+{
+    public class UnitUpdateTemplateService : ExcelTemplateBase
+    {
+        private readonly ILangueageService _lang;
+        public UnitUpdateTemplateService(ILangueageService lang) : base("UpdateUnitTemplate", lang, "UpdateUnit")
+        {
+            _lang = lang;
+        }
+
+        public override Task FillWorkSheetWithData(IXLWorksheet worksheet)
+        {
+            Func<string, string> header = (key) =>
+            {
+                return _lang[$"{BulkUploadSettings.BulkUploadCommand}.{key}.ColumnHeader"];
+            };
+
+            worksheet.Cells("A1").Value = header("Id");
+            worksheet.Cells("B1").Value = header("ItemUnitName");
+
+            return base.FillWorkSheetWithData(worksheet);
+        }
+    }
+}
