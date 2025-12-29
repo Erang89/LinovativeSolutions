055eca5bb7d01823b8cc2d438a4de9bf144fe60a
Vinsensius Pance <Vinsensius_P908@mitrais.com>
2025-12-29 14:38:55 +0800

    Create Component for Province

---
+++ Linovative.Frontend.Services/FrontendServices/ProvinceService.cs
@@ -0,0 +1,21 @@
+﻿using Linovative.Frontend.Services.FrontendServices.BaseServices;
+using Linovative.Frontend.Services.Interfaces;
+using LinoVative.Shared.Dto.Sources;
+using Microsoft.Extensions.Logging;
+
+namespace Linovative.Frontend.Services.FrontendServices
+{
+    public interface IProvinceService : IReadOnlyService<ProvinceDto> {
+        
+    }
+
+    public class ProvinceService : ReadOnlyServiceAbstract<ProvinceDto>, IProvinceService
+    {
+        public ProvinceService(IHttpClientFactory httpFactory, ILogger<ProvinceService> logger) : base(httpFactory, logger, "provinces")
+        {
+            
+        }
+
+        protected override bool IsPublicEndpoint => true;
+    }
+}
